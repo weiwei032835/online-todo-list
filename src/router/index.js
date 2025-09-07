efce064ad7ece2706e4d5d385ef3c60d3f37b214
wei <wiwei032835@gmail.com>
2025-09-07 17:39:42 +0800

    修改dist 路徑

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/vue3todolist/'),
     routes: [
         {
             path: '/',
