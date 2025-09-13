c402179dbfa50261b3fed6041e0d08caf056eca0
wei <wiwei032835@gmail.com>
2025-09-13 23:10:09 +0800

    修改createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-﻿import { createRouter, createWebHistory } from 'vue-router'
+﻿import { createRouter, createWebHashHistory } from 'vue-router'
 import TodoListView from '../views/TodoListView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 
 const router = createRouter({
-  history: createWebHistory('/vue3todolist/'),
+  history: createWebHashHistory('/vue3todolist/'),
     routes: [
         {
             path: '/',
